eb15532769354b5c2750c7981f342a3f1cbf7a50
Lidianeangelo <lidiane-angelo@hotmail.com>
2022-11-09 09:27:04 -0300

    [Criação e estilização da página Home.tsx e Home.css] 09.11.2022

---
+++ Front-End/blog-pessoal/src/paginas/home/home.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Box, Button, Grid, Typography } from '@mui/material';
+
+import "./home.css";
+
+function home() {
+    return (
+        <>
+            <Grid container justifyContent="center" className="container">
+                <Grid alignItems="center" item xs={6}>
+                    <img src="https://i.imgur.com/taqOl4k.png" alt="Logo da página" className="img" />
+                </Grid>
+                <Grid item xs={6}>
+
+                    <Box paddingX={15}>
+                        <Typography variant="h3" gutterBottom component="h3" align="center" className="escritos"> Confira aqui:</Typography>
+                        
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className="botao">Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid xs={12} className="postagem">
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default home;
