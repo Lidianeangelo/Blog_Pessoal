ec394cb73821d88c169f5202f8f311d317ca7482
Lidianeangelo <lidiane-angelo@hotmail.com>
2022-11-09 17:00:41 -0300

    [Inclusão da Navbar- atualização] 09.11.2022

--- Front-End/blog-pessoal/src/componentes/estaticos/navbar/navbar.tsx
+++ Front-End/blog-pessoal/src/componentes/estaticos/navbar/navbar.tsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { Box } from '@mui/material';
@@ -23,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function ButtonAppBar() {
+export default function NavBar() {
   const classes = useStyles();
 
   return (
