4baed46376c25a499bfc24deda2bd14d1ea7f093
Lidianeangelo <lidiane-angelo@hotmail.com>
2022-11-20 20:32:58 -0300

    [Adicionando pagina Login - atualização] 20.11.2022

--- Front-End/blog-pessoal/src/paginas/login/login.tsx
+++ Front-End/blog-pessoal/src/paginas/login/login.tsx
@@ -6,7 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import UsuarioLogin from "../../models/UsuarioLogin";
 import { login } from "../../service/Service";
 import { addToken } from "../../store/tokens/actions";
-import "./Login.css";
+import "./login.css";
 
 function Login() {
 
